d9a212f833ebc8861dba7bafe1e6534e0fcaeec7
johnny <jakasperczak@gmail.com>
2022-04-06 19:56:24 +0200

    fixed duplicate

--- lab2/openssl_folder/final/open.sh
+++ lab2/openssl_folder/final/open.sh
@@ -3,7 +3,7 @@
 openssl req -x509 -newkey rsa:4096 -days 3650 -keyout ca-key.pem -out ca-cert.pem -sha256 -subj "/C=PL/ST=DOL/L=Wr/O=PWr/OU=WIT/CN=Jonatan_Kasperczak/emailAddress=259418@student.pwr.edu.pl"
 
 #Generate private sign request with atributes
-openssl req -newkey rsa:4096 -addext "extendedKeyUsage = emailProtection,clientAuth" -keyform PEM -keyout server-key.pem -out server-req.csr -outform PEM -sha256 -subj "/C=Pol/ST=Dolno/L=Wro/O=PolWro/OU=WiIT/CN=Jonatan_Kasperczak/emailAddress=259418@student.pwr.edu.pl"
+openssl req -newkey rsa:4096 -addext "extendedKeyUsage = emailProtection,clientAuth" -keyform PEM -keyout server-key.pem -out server-req.csr -outform PEM -sha256 -subj "/C=EN/ST=ZS/L=ZS/O=Wr/OU=/CN=Kasperczak/emailAddress=259418"
 
 #Sign certificate
 openssl x509 -req -days 365 -in server-req.csr -CA ca-cert.pem -CAkey ca-key.pem -CAcreateserial -out server-cert.pem -sha256
