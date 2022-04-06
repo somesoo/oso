99aff8020f46e28f49e0aaa7eaa9bb52f702139f
Jonatan Kasperczak <jakasperczak@gmail.com>
2022-04-06 19:40:33 +0200

    initial ssh conf

---
+++ lab2/openssl_folder/final/open.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#Generate CA key and CA certyficate
+openssl req -x509 -newkey rsa:4096 -days 3650 -keyout ca-key.pem -out ca-cert.pem -sha256 -subj "/C=PL/ST=DOL/L=Wr/O=PWr/OU=WIT/CN=Jonatan_Kasperczak/emailAddress=259418@student.pwr.edu.pl"
+
+#Generate private sign request with atributes
+openssl req -newkey rsa:4096 -addext "extendedKeyUsage = emailProtection,clientAuth" -keyform PEM -keyout server-key.pem -out server-req.csr -outform PEM -sha256 -subj "/C=Pol/ST=Dolno/L=Wro/O=PolWro/OU=WiIT/CN=Jonatan_Kasperczak/emailAddress=259418@student.pwr.edu.pl"
+
+#Sign certificate
+openssl x509 -req -days 365 -in server-req.csr -CA ca-cert.pem -CAkey ca-key.pem -CAcreateserial -out server-cert.pem -sha256
+
+#Convert sign-request to txt
+openssl req -noout -text -in server-req.csr > req_csr.txt
+
+#Convert signed certificate to txt
+openssl x509 -in server-cert.pem -noout -text > cert_pem.txt
+
+#Verify
+openssl verify -CAfile ca-cert.pem server-cert.pem
